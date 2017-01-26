b5652642bdb705339532ad52d0f8e6930162cc5c
Jordan <jtm245@nau.edu>
2017-01-26 15:23:01 -0600

    chet and friends draw square, circle, and triangle

--- intro/chet.py
+++ intro/chet.py
@@ -1,17 +1,33 @@
 import turtle
 
-def draw_square():
-    window = turtle.Screen()
-    window.bgcolor("blue")
+def draw_square(new_turtle):
+    for i in range(1,5):
+        new_turtle.forward(100)
+        new_turtle.right(90)
 
+def draw_art():
+    window = turtle.Screen()
+    window.bgcolor("dark grey")
+    #Chet the turtle and his attributes
     chet = turtle.Turtle()
     chet.shape("turtle")
-    count = 0
-    while (count < 4):
-        chet.forward(100)
-        chet.right(90)
-        count = count + 1
-
+    chet.color("yellow")
+    chet.speed(2)
+    #Chet is gonna draw us a square
+    draw_square(chet)
+    #Aerie, Chet's girlfriend, draws us a circle
+    aerie = turtle.Turtle()
+    aerie.shape("arrow")
+    aerie.color("red")
+    aerie.circle(100)
+    #now billy is going to draw us a triangle
+    billy = turtle.Turtle()
+    billy.shape("circle")
+    billy.color("green")
+    for i in range(1,4):
+        billy.forward(100)
+        billy.left(120)
+    #close the window when we click it
     window.exitonclick()
 
-draw_square()
+draw_art()
