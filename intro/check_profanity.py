b4a6381939eea4763128c00a9e887b9485b6a01b
Jordan <jtm245@nau.edu>
2017-01-27 12:33:40 -0600

    changed profanity check, to instead modify txt file input to pirate speech

--- intro/check_profanity.py
+++ intro/check_profanity.py
@@ -8,9 +8,15 @@ def read_text():
     check_profanity(contents_of_file)
 
 def check_profanity(text_to_check):
-    connection = urllib.urlopen('http://www.wdylike.appspot.com/?q=shot'+text_to_check)
+    connection = urllib.urlopen('http://isithackday.com/arrpi.php?text='+text_to_check)
     output = connection.read()
     print(output)
     connection.close()
+    if 'true' in output:
+        print('Ohhhh you sad a bad word!')
+    elif 'false' in output:
+        print('No bad words here!')
+    else:
+        print('I can\'t read this.')
 
 read_text()
