4fa902434ad06e937797babaca3c8a5db8cb80e0
Jordan <jtm245@nau.edu>
2017-03-06 16:38:51 -0600

    Flask stud server running on both local machine and docker image

--- dockerPython/app/main.py
+++ dockerPython/app/main.py
@@ -1,19 +1,107 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, session, render_template, request, Response
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
+
+# dummy json obj to return for testing
+jsonObj = {
+    "total_records": 2,
+    "data": [
+        {
+            "source__channel": "110770677",
+            "first_touch": "162.17.169.249",
+            "last_touch": "0",
+            "number_of_callers": "www.cascadebh.com",
+            "percent_total": "Cascade Behavioral Health"
+        },
+        {
+            "source__channel": "110770677",
+            "first_touch": "162.17.169.249",
+            "last_touch": "0",
+            "number_of_callers": "www.cascadebh.com",
+            "percent_total": "Cascade Behavioral Health"
+        }
+    ],
+    "records_end": 2,
+    "records_start": 1,
+    "page": 1,
+    "per_page": 2
+}
+
 
 @app.route("/")
 def main():
-    # This is a dummy list, 2 nested arrays containing some
-    # params and values
-    list = [
-        {'param': 'foo', 'val': 2},
-        {'param': 'bar', 'val': 10}
-    ]
-    # jsonify will do for us all the work, returning the
-    # previous data structure in JSON
-    return jsonify(results=list)
+    # set message to return
+    message = "try /getData to POST json"
+    # spits on message on load
+    return jsonify(jsonObj)
+
+
+@app.route('/getData', methods=['POST'])
+def getData():
+    # return based on request
+    if request.method == "POST":
+        json_dict = request.get_json()
+        token = json_dict['token']
+
+        if token == 'jwcziqhurlubnm0972yg0e2y1bvxweflyfzws7mc':
+            data = {
+                "total_records": 4,
+                "data": [
+                    {
+                        "Google Organic": {
+                            "total_calls": 1000,
+                            "touches": [
+                                {
+                                    "source__channel": "Google Organic",
+                                    "first_touch": "Google Organic",
+                                    "last_touch": "Psych Today",
+                                    "number_of_callers": 200,
+                                    "percent_total": 20
+                                },
+                                {
+                                    "source__channel": "Google Organic",
+                                    "first_touch": "Google Organic",
+                                    "last_touch": "Content Net",
+                                    "number_of_callers": 100,
+                                    "percent_total": 10
+                                }
+                            ]
+                        },
+                        "Facebook Organic": {
+                            "total_calls": 200,
+                            "touches": [
+                                {
+                                    "source__channel": "Facebook Organic",
+                                    "first_touch": "Facebook Organic",
+                                    "last_touch": "Psych Today",
+                                    "number_of_callers": 100,
+                                    "percent_total": 50
+                                },
+                                {
+                                    "source__channel": "Facebook Organic",
+                                    "first_touch": "Google Organic",
+                                    "last_touch": "Content Net",
+                                    "number_of_callers": 50,
+                                    "percent_total": 25
+                                }
+                            ]
+                        }
+                    }
+                ],
+                "records_end": 4,
+                "records_start": 1,
+                "page": 1,
+                "per_page": 4
+            }
+            # if the token matches return the json object
+            return jsonify(data)
+
+    else:
+        return "GET/POST NOT WORKING"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
