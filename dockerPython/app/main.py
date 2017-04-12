f46130cf9174c05c2ecdcab792addb77e4cf6d1b
Jordan <jtm245@nau.edu>
2017-04-12 16:48:55 -0500

    install textract, python pdf reader

--- dockerPython/app/main.py
+++ dockerPython/app/main.py
@@ -39,20 +39,28 @@ def getData():
                                     "first_touch": "Google Organic",
                                     "last_touch": "Psych Today",
                                     "number_of_callers": 215,
+                                    "talk_time": 777,
                                     "percent_total": 27,
                                     "calls_over2": 77,
+                                    "talk_time_over2": 154,
                                     "calls_over5": 22,
+                                    "talk_time_over5": 110
                                 },
                             {
                                     "source__channel": "Google Organic",
                                     "first_touch": "Google Organic",
                                     "last_touch": "Content Net",
                                     "number_of_callers": 109,
+                                    "talk_time": 777,
                                     "percent_total": 13,
                                     "calls_over2": 63,
+                                    "talk_time_over2": 126,
                                     "calls_over5": 17,
+                                    "talk_time_over5": 85
                                 }
-                        ]
+                        ],
+                        "records_end": 2,
+                        "records_start": 1,
                     },
                     {
                         "name": "Facebook Organic",
@@ -65,26 +73,32 @@ def getData():
                                     "first_touch": "Facebook Organic",
                                     "last_touch": "Psych Today",
                                     "number_of_callers": 110,
+                                    "talk_time": 777,
                                     "percent_total": 55,
                                     "calls_over2": 53,
+                                    "talk_time_over2": 106,
                                     "calls_over5": 12,
+                                    "talk_time_over5": 60
                                 },
                                 {
                                     "source__channel": "Facebook Organic",
                                     "first_touch": "Facebook Organic",
                                     "last_touch": "Content Net",
                                     "number_of_callers": 66,
+                                    "talk_time": 777,
                                     "percent_total": 25,
                                     "calls_over2": 33,
+                                    "talk_time_over2": 66,
                                     "calls_over5": 11,
+                                    "talk_time_over5": 55
                                 }
-                        ]
+                        ],
+                        "records_end": 2,
+                        "records_start": 1,
                     }
                 ],
-                "records_end": 4,
-                "records_start": 1,
                 "page": 1,
-                "per_page": 4
+                "per_page": 25
             }
             # if the token matches return the json object
             return jsonify(data)
