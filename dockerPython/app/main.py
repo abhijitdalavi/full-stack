18e98320ababd36c3694844a7e1d1be7bf4a9ae7
Jordan <jtm245@nau.edu>
2017-04-12 14:53:02 -0500

    updates to return json data

--- dockerPython/app/main.py
+++ dockerPython/app/main.py
@@ -24,44 +24,59 @@ def getData():
         if token == 'jwcziqhurlubnm0972yg0e2y1bvxweflyfzws7mc':
             data = {
                 "total_records": 4,
+                "total_calls": 13000,
+                "total_calls_over2": 7000,
+                "total_calls_over5": 2000,
                 "data": [
                     {
                         "name": "Google Organic",
                         "total_calls": 1000,
+                        "total_calls_over2": 777,
+                        "total_calls_over5": 212,
                         "touches": [
                                 {
                                     "source__channel": "Google Organic",
                                     "first_touch": "Google Organic",
                                     "last_touch": "Psych Today",
-                                    "number_of_callers": 200,
-                                    "percent_total": 20
+                                    "number_of_callers": 215,
+                                    "percent_total": 27,
+                                    "calls_over2": 77,
+                                    "calls_over5": 22,
                                 },
                             {
                                     "source__channel": "Google Organic",
                                     "first_touch": "Google Organic",
                                     "last_touch": "Content Net",
-                                    "number_of_callers": 100,
-                                    "percent_total": 10
+                                    "number_of_callers": 109,
+                                    "percent_total": 13,
+                                    "calls_over2": 63,
+                                    "calls_over5": 17,
                                 }
                         ]
                     },
                     {
                         "name": "Facebook Organic",
-                        "total_calls": 200,
+                        "total_calls": 547,
+                        "total_calls_over2": 653,
+                        "total_calls_over5": 124,
                         "touches": [
                                 {
                                     "source__channel": "Facebook Organic",
                                     "first_touch": "Facebook Organic",
                                     "last_touch": "Psych Today",
-                                    "number_of_callers": 100,
-                                    "percent_total": 50
+                                    "number_of_callers": 110,
+                                    "percent_total": 55,
+                                    "calls_over2": 53,
+                                    "calls_over5": 12,
                                 },
                                 {
                                     "source__channel": "Facebook Organic",
                                     "first_touch": "Facebook Organic",
                                     "last_touch": "Content Net",
-                                    "number_of_callers": 50,
-                                    "percent_total": 25
+                                    "number_of_callers": 66,
+                                    "percent_total": 25,
+                                    "calls_over2": 33,
+                                    "calls_over5": 11,
                                 }
                         ]
                     }
