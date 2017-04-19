baa2b21627c7f7eaceebb77d062d3ae3dd511886
Jordan <jtm245@nau.edu>
2017-04-19 16:42:30 -0500

    updated dummy json object to reflect view in channels

--- knick-knacks/json-csv.js
+++ knick-knacks/json-csv.js
@@ -1,92 +1,61 @@
+/*
+    Just a simple solution to the "Quick Export" functionality I needed.
+    All you gotta do is pass in a JSON object and click the button!
+*/
+
 // Just a snippet sample from YAK (channels)
-var dummyData = {
-                "total_records": 4,
-                "total_calls": 13000,
-                "total_calls_over2": 7000,
-                "total_calls_over5": 2000,
-                "data": [
-                    {
-                        "name": "Google Organic",
-                        "total_calls": 1000,
-                        "total_calls_over2": 777,
-                        "total_calls_over5": 212,
-                        "touches": [
-                                {
-                                    "source__channel": "Google Organic",
-                                    "first_touch": "Google Organic",
-                                    "last_touch": "Psych Today",
-                                    "number_of_callers": 215,
-                                    "talk_time": 777,
-                                    "percent_total": 27,
-                                    "calls_over2": 77,
-                                    "talk_time_over2": 154,
-                                    "calls_over5": 22,
-                                    "talk_time_over5": 110
-                                },
-                            {
-                                    "source__channel": "Google Organic",
-                                    "first_touch": "Google Organic",
-                                    "last_touch": "Content Net",
-                                    "number_of_callers": 109,
-                                    "talk_time": 777,
-                                    "percent_total": 13,
-                                    "calls_over2": 63,
-                                    "talk_time_over2": 126,
-                                    "calls_over5": 17,
-                                    "talk_time_over5": 85
-                                }
-                        ],
-                        "records_end": 2,
-                        "records_start": 1,
-                    },
-                    {
-                        "name": "Facebook Organic",
-                        "total_calls": 547,
-                        "total_calls_over2": 653,
-                        "total_calls_over5": 124,
-                        "touches": [
-                                {
-                                    "source__channel": "Facebook Organic",
-                                    "first_touch": "Facebook Organic",
-                                    "last_touch": "Psych Today",
-                                    "number_of_callers": 110,
-                                    "talk_time": 777,
-                                    "percent_total": 55,
-                                    "calls_over2": 53,
-                                    "talk_time_over2": 106,
-                                    "calls_over5": 12,
-                                    "talk_time_over5": 60
-                                },
-                                {
-                                    "source__channel": "Facebook Organic",
-                                    "first_touch": "Facebook Organic",
-                                    "last_touch": "Content Net",
-                                    "number_of_callers": 66,
-                                    "talk_time": 777,
-                                    "percent_total": 25,
-                                    "calls_over2": 33,
-                                    "talk_time_over2": 66,
-                                    "calls_over5": 11,
-                                    "talk_time_over5": 55
-                                }
-                        ],
-                        "records_end": 2,
-                        "records_start": 1,
-                    }
-                ],
-                "page": 1,
-                "per_page": 25
-            };
+var dummyData = [
+    {
+    "Datetime": "3:00",
+    "1st Touch": 23423,
+    "Last Before Call": 7233200,
+    "# of Callers": 202340,
+    "Calls > 2min": 323,
+    "Calls > 5min": 123,
+    "Talk Time": 250000,
+    "% Total": 56
+},
+{
+    "Datetime": "11:00",
+    "1st Touch": 3456,
+    "Last Before Call": 456,
+    "# of Callers": 73736,
+    "Calls > 2min": 385,
+    "Calls > 5min": 145,
+    "Talk Time": 85546,
+    "% Total": 27
+},
+{
+    "Datetime": "16:00",
+    "1st Touch": 8745,
+    "Last Before Call": 4456,
+    "# of Callers": 2756,
+    "Calls > 2min": 345,
+    "Calls > 5min": 174,
+    "Talk Time": 4546,
+    "% Total": 19
+},
+{
+    "Datetime": "7:77",
+    "1st Touch": 9834,
+    "Last Before Call": 9834,
+    "# of Callers": 93847,
+    "Calls > 2min": 234,
+    "Calls > 5min": 451,
+    "Talk Time": 349874,
+    "% Total": 8
+}
+];
 
 // The converter function, this is where the magic happens
 function ConvertToCSV(jsonIn) {
 
     // Variables we gon' need.
     var result, ctr, keys, columnDelim, lineDelim, data;
-    
+
     // Set data var to data input, otherwise nothing
     data = jsonIn.data || null;
-    if (data == null || !data.length){
+    if (data == null || !data.length) {
         // If nothing, tell them they didn't give us anything!
         return "No Data!";
     }
@@ -106,13 +75,13 @@ function ConvertToCSV(jsonIn) {
     result += lineDelim;
 
     // Loop through each data object
-    data.forEach(function(item) {
+    data.forEach(function (item) {
 
         // Init counter
         ctr = 0;
 
         // Iterate through each value in said object
-        keys.forEach(function(key){
+        keys.forEach(function (key) {
             if (ctr > 0) result += columnDelim;
             result += item[key];
             ctr++;
@@ -127,8 +96,8 @@ function ConvertToCSV(jsonIn) {
 }
 
 // Download function, because how else they gonna get it?
-function downloadCSV(stuff) {
-    
+function downloadCSV(input) {
+
     // We gon' need these
     var data, filename, link;
 
@@ -136,11 +105,12 @@ function downloadCSV(stuff) {
     var csv = ConvertToCSV({
         data: dummyData
     });
+
     // If nothing comes back we can just start right here...
     if (csv == null) return;
 
     // Check for filename, otherwise give 'em one
-    filename = stuff.filename || 'export.csv';
+    filename = input.filename || 'export.csv';
 
     // Magic that tells browser this thing is a CSV
     if (!csv.match(/^data:text\/csv/i)) {
